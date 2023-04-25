48a1919ead5acbbd7f63080935535d21fb65a5f0
wilsonleung767 <66161967+wilsonleung767@users.noreply.github.com>
2023-04-25 10:05:11 +0800

    finished langchain

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,18 +1,18 @@
 import openai
 # from pydub import AudioSegment
 import os
-openai.api_key = "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"
+openai.api_key = "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"
 
-# MP3_file = "audio\cutVer.mp3"
-# file_name = os.path.basename(MP3_file)
-# file_name = (file_name.split('.'))[0]
+MP3_file = "audio\cutVer.mp3"
+file_name = os.path.basename(MP3_file)
+file_name = (file_name.split('.'))[0]
 
-# audio_file= open(MP3_file, "rb")
-# transcript = openai.Audio.translate("whisper-1", audio_file)
+audio_file= open(MP3_file, "rb")
+transcript = openai.Audio.translate("whisper-1", audio_file)
 
-# with open(f'script/{file_name}.txt', 'w') as f:
-#     f.write(transcript.text)
-# print("script generated")
+with open(f'script/{file_name}.txt', 'w') as f:
+    f.write(f"{transcript.text} \n")
+print("script generated")
 
 with open("script\cutVer.txt", 'r') as f:
     script_text = f.read()
@@ -21,10 +21,11 @@ chat = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
         {"role": "system", "content": "You are a helpful assistant that can help me summerize the content of a podcast script."},
-        {"role": "user", "content": f"Summerize the content of the podcast, list them in point form, keep it detailed and clear.This is the script of the podcast: {script_text}" }
+        {"role": "user", "content": f"Summerize the content of the podcast, list the summary in point form, keep it detailed and clear.This is the script of the podcast: {script_text}" }
+
     ]
 )
-print(chat.choices[0].message.content)
+print(chat.choices[1].message.content)
 # audio = AudioSegment.from_mp3(MP3_file)
 
 # # PyDub handles time in milliseconds
