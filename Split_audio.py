590952d3dc75525d28945a5d0c35df9e04693b2f
wilsonleung767 <wilsonleung767@gmail.com>
2023-04-29 23:05:24 +0800

    Done splitaudio and audiototext

---
+++ Split_audio.py
@@ -0,0 +1,29 @@
+from pydub import AudioSegment
+import os
+import math
+
+def split_audio( mp3_file, minute_interval):
+    file_name = os.path.basename(mp3_file)
+    filename = os.path.splitext(file_name)[0]
+    audio = AudioSegment.from_mp3(mp3_file)
+
+    # PyDub handles time in milliseconds
+    minute_interval = minute_interval * 60 * 1000
+    segment_10_mins = audio[:minute_interval]
+    # Calculate the number of segments
+    num_segments = math.ceil(len(audio)/minute_interval)
+    # Split the audio into segments
+    segments = [audio[i*minute_interval:(i+1)*minute_interval] for i in range(num_segments)]
+
+
+    # Create a new folder when there is no existing folder
+    folder_path = f'audio/{filename}'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    # Generating splited audio
+    for i, segment in enumerate(segments):
+        segment.export(f"{folder_path}/{filename}_A{i}.mp3", format="mp3")
+
+    print("Audio splitted")
+
--- audio_to_text.py
+++ audio_to_text.py
@@ -1,36 +1,42 @@
-import openai
-# from pydub import AudioSegment
+# import openai
 import os
-openai.api_key = "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"
+from Split_audio import split_audio
+# openai.api_key = "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"
+def audio_to_text(mp3_file):
+# mp3_file = 'audio/podcast/podcast.mp3'
+    split_audio(mp3_file,10)
+    file = os.path.basename(mp3_file)
+    file_name = os.path.splitext(file)[0]
 
-MP3_file = "audio\cutVer.mp3"
-file_name = os.path.basename(MP3_file)
-file_name = (file_name.split('.'))[0]
+    # file_name = 'podcast'
+    audio_folder = f'audio/{file_name}'
+    audio_files = os.listdir(audio_folder)
 
-audio_file= open(MP3_file, "rb")
-transcript = openai.Audio.translate("whisper-1", audio_file)
+    for i , file in enumerate(audio_files):
+        # check if the file is an audio file
+        # if file.endswith(".mp3") or file.endswith(".wav") or file.endswith(".ogg"):
+            # audio_for_translation = open(file, "rb")
+            # transcript = openai.Audio.translate("whisper-1", file)
+        # Create a new folder when there is no existing folder
+        script_folder_path = f'script/{file_name}'
+        file_inside_folder = os.path.basename(file)
+        file_inside_name = os.path.splitext(file_inside_folder)
 
-with open(f'script/{file_name}.txt', 'w') as f:
-    f.write(f"{transcript.text} \n")
-print("script generated")
+        if not os.path.exists(script_folder_path):
+            os.makedirs(script_folder_path)
+        with open(f'{script_folder_path}/{file_inside_name}_S{i}.txt', 'w') as f:
+            f.write(f"yes \n")
+        print(f"script {i} generated")
+audio_to_text('audio/podcast.mp3')        
+# with open("script\cutVer.txt", 'r') as f:
+#     script_text = f.read()
 
-with open("script\cutVer.txt", 'r') as f:
-    script_text = f.read()
+# chat = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#         {"role": "system", "content": "You are a helpful assistant that can help me summerize the content of a podcast script."},
+#         {"role": "user", "content": f"Summerize the content of the podcast, list the summary in point form, keep it detailed and clear.This is the script of the podcast: {script_text}" }
 
-chat = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "You are a helpful assistant that can help me summerize the content of a podcast script."},
-        {"role": "user", "content": f"Summerize the content of the podcast, list the summary in point form, keep it detailed and clear.This is the script of the podcast: {script_text}" }
-
-    ]
-)
-print(chat.choices[1].message.content)
-# audio = AudioSegment.from_mp3(MP3_file)
-
-# # PyDub handles time in milliseconds
-# ten_minutes = 10 * 60 * 1000
-
-# first_10_minutes = audio[:ten_minutes]
-
-# first_10_minutes.export("result_10mins", format="mp3")
+#     ]
+# )
+# print(chat.choices[1].message.content)
